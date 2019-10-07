234230a62486ed80f43f7fc028491350a4a2bd5d
pablo correa <juanpabloc2000@gmail.com>
2019-10-07 17:48:29 -0500

    first commit needs twobytwo

---
+++ Assets/Scripts/CardsControler.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardsControler : MonoBehaviour
+{
+    public bool show = false;
+    public int textureRand;
+    public static int[] counters = new int[8];
+
+    void Start()
+    {
+        textureRand = Random.Range(0,8);
+    }
+
+    void Hide()
+    {
+        show = false;
+    }
+
+    void OnMouseDown()
+    {
+        show = true;
+        Invoke("Hide",2);
+    }
+
+    private void Update() 
+    {
+        switch (textureRand)
+        {
+        case 0:
+            if (counters[0] < 2)
+            {
+            gameObject.GetComponent<Renderer>().material = General.staticMaterial1;
+            ++counters[0];
+            }
+            else
+            textureRand = Random.Range(0,8);
+            break;
+        case 1:
+            if (counters[1] < 2)
+            {
+            gameObject.GetComponent<Renderer>().material = General.staticMaterial2;
+            ++counters[1];
+            }
+            else
+            textureRand = Random.Range(0,8);
+            break;
+        case 2:
+            if (counters[2] < 2)
+            {
+            gameObject.GetComponent<Renderer>().material = General.staticMaterial3;
+            ++counters[2];
+            }
+            else
+            textureRand = Random.Range(0,8);
+            break;
+        case 3:
+            if (counters[3] < 2)
+            {
+            gameObject.GetComponent<Renderer>().material = General.staticMaterial4;
+            ++counters[3];
+            }
+            else
+            textureRand = Random.Range(0,8);
+            break;
+        case 4:
+            if (counters[4] < 2)
+            {
+            gameObject.GetComponent<Renderer>().material = General.staticMaterial5;
+            ++counters[4];
+            }
+            else
+            textureRand = Random.Range(0,8);
+            break;
+        case 5:
+            if (counters[5] < 2)
+            {
+            gameObject.GetComponent<Renderer>().material = General.staticMaterial6;
+            ++counters[5];
+            }
+            else
+            textureRand = Random.Range(0,8);
+            break;
+        case 6:
+            if (counters[6] < 2)
+            {
+            gameObject.GetComponent<Renderer>().material = General.staticMaterial7;
+            ++counters[6];
+            }
+            else
+            textureRand = Random.Range(0,8);
+            break;
+        case 7:
+            if (counters[7] < 2)
+            {
+            gameObject.GetComponent<Renderer>().material = General.staticMaterial8;
+            ++counters[7];
+            }
+            else
+            textureRand = Random.Range(0,8);
+            break;
+        }
+
+        if (show)
+            transform.rotation = Quaternion.Euler(Vector3.Lerp(transform.rotation.eulerAngles, Vector3.up * 180, 2 * Time.deltaTime));
+        else
+            transform.rotation = Quaternion.Euler(Vector3.Lerp(transform.rotation.eulerAngles, Vector3.zero, 2 * Time.deltaTime));
+    }
+}
---
+++ Assets/Scripts/General.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class General : MonoBehaviour
+{
+    public Material visibleMaterial1;
+    public Material visibleMaterial2;
+    public Material visibleMaterial3;
+    public Material visibleMaterial4;
+    public Material visibleMaterial5;
+    public Material visibleMaterial6;
+    public Material visibleMaterial7;
+    public Material visibleMaterial8;
+
+    public static Material staticMaterial1;
+    public static Material staticMaterial2;
+    public static Material staticMaterial3;
+    public static Material staticMaterial4;
+    public static Material staticMaterial5;
+    public static Material staticMaterial6;
+    public static Material staticMaterial7;
+    public static Material staticMaterial8;
+
+    private void Awake() 
+    {
+        staticMaterial1 = visibleMaterial1;
+        staticMaterial2 = visibleMaterial2;
+        staticMaterial3 = visibleMaterial3;
+        staticMaterial4 = visibleMaterial4;
+        staticMaterial5 = visibleMaterial5;
+        staticMaterial6 = visibleMaterial6;
+        staticMaterial7 = visibleMaterial7;
+        staticMaterial8 = visibleMaterial8;
+    }
+}
