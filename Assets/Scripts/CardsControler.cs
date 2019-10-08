b7a9e0ac113fb4409595df308e72f6250b979dca
pablo correa <juanpabloc2000@gmail.com>
2019-10-08 16:04:28 -0500

    verify works! but the coroutine no works

--- Assets/Scripts/CardsControler.cs
+++ Assets/Scripts/CardsControler.cs
@@ -8,6 +8,7 @@ public class CardsControler : MonoBehaviour
     public bool materialFinded = false;
     public int textureRand;
     public static int[] counters = new int[8];
+    public CubesID mID;
 
     void Start()
     {
@@ -24,7 +25,34 @@ public class CardsControler : MonoBehaviour
     void OnMouseDown()
     {
         show = true;
+        if (General.cubesVerificator[0] == null)
+        General.cubesVerificator[0] = gameObject;
+        else
+        {
+            General.cubesVerificator[1] = gameObject;
+            Verification();
+        }
         Invoke("Hide",4);
+    }
+
+    public void Verification()
+    {
+        if (General.cubesVerificator[0].GetComponent<CardsControler>().mID == General.cubesVerificator[1].GetComponent<CardsControler>().mID)
+        {
+            StartCoroutine(CubeDestroyer(General.cubesVerificator[0].gameObject, General.cubesVerificator[1].gameObject));
+        }
+        else
+        {
+            General.cubesVerificator[0] = null;
+            General.cubesVerificator[1] = null;
+        }
+    }
+
+    IEnumerator CubeDestroyer(GameObject uno, GameObject dos)
+    {
+        yield return new WaitForSeconds(0.5f);
+        Destroy(uno);
+        Destroy(dos);
     }
 
     IEnumerator MaterialAssigner()
@@ -36,10 +64,10 @@ public class CardsControler : MonoBehaviour
                 case 0:
                     if (counters[0] < 2)
                     {
-                        gameObject.GetComponent<Renderer>().material = General.staticMaterial1;
+                        gameObject.GetComponent<Renderer>().material = General.staticMaterials[0];
                         ++counters[0];
                         materialFinded = true;
-                        StopCoroutine(MaterialAssigner());
+                        mID = CubesID.C1;
                     }
                     else
                         textureRand = Random.Range(0, 8);
@@ -47,10 +75,10 @@ public class CardsControler : MonoBehaviour
                 case 1:
                     if (counters[1] < 2)
                     {
-                        gameObject.GetComponent<Renderer>().material = General.staticMaterial2;
+                        gameObject.GetComponent<Renderer>().material = General.staticMaterials[1];
                         ++counters[1];
                         materialFinded = true;
-                        StopCoroutine(MaterialAssigner());
+                        mID = CubesID.C2;
                     }
                     else
                         textureRand = Random.Range(0, 8);
@@ -58,10 +86,10 @@ public class CardsControler : MonoBehaviour
                 case 2:
                     if (counters[2] < 2)
                     {
-                        gameObject.GetComponent<Renderer>().material = General.staticMaterial3;
+                        gameObject.GetComponent<Renderer>().material = General.staticMaterials[2];
                         ++counters[2];
                         materialFinded = true;
-                        StopCoroutine(MaterialAssigner());
+                        mID = CubesID.C3;
                     }
                     else
                         textureRand = Random.Range(0, 8);
@@ -69,10 +97,10 @@ public class CardsControler : MonoBehaviour
                 case 3:
                     if (counters[3] < 2)
                     {
-                        gameObject.GetComponent<Renderer>().material = General.staticMaterial4;
+                        gameObject.GetComponent<Renderer>().material = General.staticMaterials[3];
                         ++counters[3];
                         materialFinded = true;
-                        StopCoroutine(MaterialAssigner());
+                        mID = CubesID.C4;
                     }
                     else
                         textureRand = Random.Range(0, 8);
@@ -80,10 +108,10 @@ public class CardsControler : MonoBehaviour
                 case 4:
                     if (counters[4] < 2)
                     {
-                        gameObject.GetComponent<Renderer>().material = General.staticMaterial5;
+                        gameObject.GetComponent<Renderer>().material = General.staticMaterials[4];
                         ++counters[4];
                         materialFinded = true;
-                        StopCoroutine(MaterialAssigner());
+                        mID = CubesID.C5;
                     }
                     else
                         textureRand = Random.Range(0, 8);
@@ -91,10 +119,10 @@ public class CardsControler : MonoBehaviour
                 case 5:
                     if (counters[5] < 2)
                     {
-                        gameObject.GetComponent<Renderer>().material = General.staticMaterial6;
+                        gameObject.GetComponent<Renderer>().material = General.staticMaterials[5];
                         ++counters[5];
                         materialFinded = true;
-                        StopCoroutine(MaterialAssigner());
+                        mID = CubesID.C6;
                     }
                     else
                         textureRand = Random.Range(0, 8);
@@ -102,10 +130,10 @@ public class CardsControler : MonoBehaviour
                 case 6:
                     if (counters[6] < 2)
                     {
-                        gameObject.GetComponent<Renderer>().material = General.staticMaterial7;
+                        gameObject.GetComponent<Renderer>().material = General.staticMaterials[6];
                         ++counters[6];
                         materialFinded = true;
-                        StopCoroutine(MaterialAssigner());
+                        mID = CubesID.C7;
                     }
                     else
                         textureRand = Random.Range(0, 8);
@@ -113,10 +141,10 @@ public class CardsControler : MonoBehaviour
                 case 7:
                     if (counters[7] < 2)
                     {
-                        gameObject.GetComponent<Renderer>().material = General.staticMaterial8;
+                        gameObject.GetComponent<Renderer>().material = General.staticMaterials[7];
                         ++counters[7];
                         materialFinded = true;
-                        StopCoroutine(MaterialAssigner());
+                        mID = CubesID.C8;
                     }
                     else
                         textureRand = Random.Range(0, 8);
@@ -135,5 +163,9 @@ public class CardsControler : MonoBehaviour
             transform.rotation = Quaternion.Euler(Vector3.Lerp(transform.rotation.eulerAngles, Vector3.up * 180, 2 * Time.deltaTime));
         else
             transform.rotation = Quaternion.Euler(Vector3.Lerp(transform.rotation.eulerAngles, Vector3.zero, 2 * Time.deltaTime));
+
+        
     }
 }
+
+public enum CubesID {C1,C2,C3,C4,C5,C6,C7,C8}
--- Assets/Scripts/General.cs
+++ Assets/Scripts/General.cs
@@ -4,33 +4,15 @@ using UnityEngine;
 
 public class General : MonoBehaviour
 {
-    public Material visibleMaterial1;
-    public Material visibleMaterial2;
-    public Material visibleMaterial3;
-    public Material visibleMaterial4;
-    public Material visibleMaterial5;
-    public Material visibleMaterial6;
-    public Material visibleMaterial7;
-    public Material visibleMaterial8;
-
-    public static Material staticMaterial1;
-    public static Material staticMaterial2;
-    public static Material staticMaterial3;
-    public static Material staticMaterial4;
-    public static Material staticMaterial5;
-    public static Material staticMaterial6;
-    public static Material staticMaterial7;
-    public static Material staticMaterial8;
+    public Material[] visibleMaterials = new Material[8];
+    public static Material[] staticMaterials = new Material[8];
+    public static GameObject[] cubesVerificator = new GameObject[2];
 
     private void Awake() 
     {
-        staticMaterial1 = visibleMaterial1;
-        staticMaterial2 = visibleMaterial2;
-        staticMaterial3 = visibleMaterial3;
-        staticMaterial4 = visibleMaterial4;
-        staticMaterial5 = visibleMaterial5;
-        staticMaterial6 = visibleMaterial6;
-        staticMaterial7 = visibleMaterial7;
-        staticMaterial8 = visibleMaterial8;
+        for (int i = 0; i < 8; i++)
+        {
+        staticMaterials[i] = visibleMaterials[i];
+        }
     }
 }
