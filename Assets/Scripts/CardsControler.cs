67df9a2e6d82f3434052e9c00fa85cf677565bc9
pablo correa <juanpabloc2000@gmail.com>
2019-10-07 20:47:12 -0500

    trying coroutine without wait for seconds

--- Assets/Scripts/CardsControler.cs
+++ Assets/Scripts/CardsControler.cs
@@ -21,86 +21,102 @@ public class CardsControler : MonoBehaviour
     void OnMouseDown()
     {
         show = true;
-        Invoke("Hide",2);
+        Invoke("Hide",4);
+    }
+
+    IEnumerator MaterialAssigner()
+    {
+        while (true)
+        {
+            switch (textureRand)
+            {
+                case 0:
+                    if (counters[0] < 2)
+                    {
+                        gameObject.GetComponent<Renderer>().material = General.staticMaterial1;
+                        ++counters[0];
+                        StopCoroutine(MaterialAssigner());
+                    }
+                    else
+                        textureRand = Random.Range(0, 8);
+                    break;
+                case 1:
+                    if (counters[1] < 2)
+                    {
+                        gameObject.GetComponent<Renderer>().material = General.staticMaterial2;
+                        ++counters[1];
+                        StopCoroutine(MaterialAssigner());
+                    }
+                    else
+                        textureRand = Random.Range(0, 8);
+                    break;
+                case 2:
+                    if (counters[2] < 2)
+                    {
+                        gameObject.GetComponent<Renderer>().material = General.staticMaterial3;
+                        ++counters[2];
+                        StopCoroutine(MaterialAssigner());
+                    }
+                    else
+                        textureRand = Random.Range(0, 8);
+                    break;
+                case 3:
+                    if (counters[3] < 2)
+                    {
+                        gameObject.GetComponent<Renderer>().material = General.staticMaterial4;
+                        ++counters[3];
+                        StopCoroutine(MaterialAssigner());
+                    }
+                    else
+                        textureRand = Random.Range(0, 8);
+                    break;
+                case 4:
+                    if (counters[4] < 2)
+                    {
+                        gameObject.GetComponent<Renderer>().material = General.staticMaterial5;
+                        ++counters[4];
+                        StopCoroutine(MaterialAssigner());
+                    }
+                    else
+                        textureRand = Random.Range(0, 8);
+                    break;
+                case 5:
+                    if (counters[5] < 2)
+                    {
+                        gameObject.GetComponent<Renderer>().material = General.staticMaterial6;
+                        ++counters[5];
+                        StopCoroutine(MaterialAssigner());
+                    }
+                    else
+                        textureRand = Random.Range(0, 8);
+                    break;
+                case 6:
+                    if (counters[6] < 2)
+                    {
+                        gameObject.GetComponent<Renderer>().material = General.staticMaterial7;
+                        ++counters[6];
+                        StopCoroutine(MaterialAssigner());
+                    }
+                    else
+                        textureRand = Random.Range(0, 8);
+                    break;
+                case 7:
+                    if (counters[7] < 2)
+                    {
+                        gameObject.GetComponent<Renderer>().material = General.staticMaterial8;
+                        ++counters[7];
+                        StopCoroutine(MaterialAssigner());
+                    }
+                    else
+                        textureRand = Random.Range(0, 8);
+                    break;
+            }
+        }
     }
 
     private void Update() 
     {
-        switch (textureRand)
-        {
-        case 0:
-            if (counters[0] < 2)
-            {
-            gameObject.GetComponent<Renderer>().material = General.staticMaterial1;
-            ++counters[0];
-            }
-            else
-            textureRand = Random.Range(0,8);
-            break;
-        case 1:
-            if (counters[1] < 2)
-            {
-            gameObject.GetComponent<Renderer>().material = General.staticMaterial2;
-            ++counters[1];
-            }
-            else
-            textureRand = Random.Range(0,8);
-            break;
-        case 2:
-            if (counters[2] < 2)
-            {
-            gameObject.GetComponent<Renderer>().material = General.staticMaterial3;
-            ++counters[2];
-            }
-            else
-            textureRand = Random.Range(0,8);
-            break;
-        case 3:
-            if (counters[3] < 2)
-            {
-            gameObject.GetComponent<Renderer>().material = General.staticMaterial4;
-            ++counters[3];
-            }
-            else
-            textureRand = Random.Range(0,8);
-            break;
-        case 4:
-            if (counters[4] < 2)
-            {
-            gameObject.GetComponent<Renderer>().material = General.staticMaterial5;
-            ++counters[4];
-            }
-            else
-            textureRand = Random.Range(0,8);
-            break;
-        case 5:
-            if (counters[5] < 2)
-            {
-            gameObject.GetComponent<Renderer>().material = General.staticMaterial6;
-            ++counters[5];
-            }
-            else
-            textureRand = Random.Range(0,8);
-            break;
-        case 6:
-            if (counters[6] < 2)
-            {
-            gameObject.GetComponent<Renderer>().material = General.staticMaterial7;
-            ++counters[6];
-            }
-            else
-            textureRand = Random.Range(0,8);
-            break;
-        case 7:
-            if (counters[7] < 2)
-            {
-            gameObject.GetComponent<Renderer>().material = General.staticMaterial8;
-            ++counters[7];
-            }
-            else
-            textureRand = Random.Range(0,8);
-            break;
-        }
+        
 
         if (show)
             transform.rotation = Quaternion.Euler(Vector3.Lerp(transform.rotation.eulerAngles, Vector3.up * 180, 2 * Time.deltaTime));
