03dd80093d1f5d7a05a64d7995dc5bc26c46776f
pablo correa <juanpabloc2000@gmail.com>
2019-10-09 16:28:03 -0500

    implementing hard level

---
+++ Assets/Scripts/ButtonsManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonsManager : MonoBehaviour
+{
+    public void HardLevel()
+    {
+        SceneManager.LoadScene("HardGameScene");
+    }
+    public void EasyLevel()
+    {
+        SceneManager.LoadScene("EasyGameScene");
+    }
+}
